71e4df6862213851320563f385509c33377c2db7
gprimo <glaucoprimo@gmail.com>
2021-02-13 14:36:14 -0300

    todos os arquivos iniciais

---
+++ src/test/java/com/ponto/inteligente/InteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ponto.inteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteligenteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
